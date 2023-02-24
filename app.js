985db1a06ef8ee094b1a21a2d28823948435f55e
dimakuznetsow <108948435+dimakuznetsow@users.noreply.github.com>
2023-02-24 12:55:19 +0200

    Part I - Setup

---
+++ app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+
+// express app
+const app = express();
+
+// port number
+const port = process.env.PORT || 3003;
+
+// middleware
+app.use(express.json());
+
+// route
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Unsplash API!" });
+});
+
+// server configuration
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+});
